bd0c63a045b97ceae38f10b762fa1d06709fdeaf
redstonetehnik <redstone.tehnik@gmail.com>
2017-01-07 17:39:15 +0100

    Added potentiometer option

--- DetectorTx/DetectorTx.ino
+++ DetectorTx/DetectorTx.ino
@@ -3,19 +3,27 @@
  This is transmitter code, it just sends pulses every .3 seconds.
  */
 const int tx_pin = 12;
-int delay_time = 30
-
+int delay_time = 30;
+const int pot_pin = 11;
+const char msgChar[] =  "A";
 
 void setup()
 {
  vw_set_tx_pin  (uint8_t tx_pin);
+ pinMode(pot_pin, OUTPUT);
 }
 
 void loop()
 {
-  ...
-  const char msgChar[] =  "A";
- 
+  delay_time = pot_read(); //comment this line if potentiometer is not used/needed
   vw_send((uint8_t *)msgChar, strlen(msgChar));
   delay (delay_time);
 }
+
+void pot_read()
+{
+  int pot = analogRead(pot_pin);
+  pot = map(pot, 0, 1023, 50, 0);
+  return(pot);
+}
+
